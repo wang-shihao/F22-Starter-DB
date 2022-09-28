474e61862ceff9e1264aab17a0b5d81df1ca39cd
Arthur, Shihao Wang <justinwangshihao@gmail.com>
2022-09-28 12:08:05 -0400

    Modified code to add my uni

---
+++ some_columbia_student_sql.sql
@@ -0,0 +1,26 @@
+/*
+ Create table statement.
+ */
+create table columbia_student
+(
+    auto_id     int auto_increment
+        primary key,
+    last_name   varchar(128) not null,
+    first_name  varchar(128) not null,
+    middle_name varchar(128) null,
+    email       varchar(256) not null,
+    uni         varchar(12)  not null,
+    constraint columbia_student_email_uindex
+        unique (email),
+    constraint columbia_student_uni_uindex
+        unique (uni)
+);
+
+/*
+ Example insert statement. Note that since auto_id is auto-increment, the insert
+ statement does not need to specify it.
+ */
+ insert into columbia_students (last_name, first_name, middle_name, email, guid, school_code)
+    values ("Wang", "Shihao", "None", "sw3567@columbia.edu", "sw3567", "SEAS");
+
+select * from columbia_students where first_name="Shihao";
